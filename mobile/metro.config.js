71a7fd242136ad8fcc4e460bf6025b3718ecb141
Juztforkickz <info@psiperformance.com.au>
2026-08-22 19:36:16 +1000

    Fix web booking date picker resolution

---
+++ mobile/metro.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+const defaultResolveRequest = config.resolver.resolveRequest;
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === 'web' && moduleName === '@react-native-community/datetimepicker') {
+    return {
+      filePath: path.resolve(__dirname, 'src/components/web-date-time-picker.tsx'),
+      type: 'sourceFile',
+    };
+  }
+
+  if (defaultResolveRequest) {
+    return defaultResolveRequest(context, moduleName, platform);
+  }
+
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+module.exports = config;
